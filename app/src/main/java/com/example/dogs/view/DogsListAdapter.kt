b3f96094f29f5430d463f352a245ad3ee966199f
Niranjan Naik <niranjanpapu4@gmail.com>
2021-05-20 00:45:40 +0530

    data binding

--- app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
+++ app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
@@ -14,7 +14,7 @@ import com.example.dogs.util.loadImage
 import kotlinx.android.synthetic.main.item_dog.view.*
 
 class DogsListAdapter(val dogList: ArrayList<DogBreed>) :
-    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>(), DogClickListener {
 
     class DogViewHolder(var view: ItemDogBinding) : RecyclerView.ViewHolder(view.root)
 
@@ -27,28 +27,24 @@ class DogsListAdapter(val dogList: ArrayList<DogBreed>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-//        val view = inflater.inflate(R.layout.item_dog, parent, false)
-        val view = DataBindingUtil.inflate<ItemDogBinding>(inflater,R.layout.item_dog,parent,false)
+        val view =
+            DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
         return DogViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
-            holder.view.dog = dogList[position]
+        holder.view.dog = dogList[position]
+        holder.view.listener = this
 
-//        holder.view.name.text = dogList[position].dogBreed
-//        holder.view.lifespan.text = dogList[position].lifeSpan
-//        holder.view.setOnClickListener {
-//            val action = ListFragmentDirections.actionDetailFragment()
-//            action.dogUuid = dogList[position].uuid
-//            Navigation.findNavController(it).navigate(action)
-//        }
-//
-//        holder.view.imageView.loadImage(
-//            dogList[position].imageUrl,
-//            getProgressDrawable(holder.view.imageView.context)
-//        )
 
     }
 
     override fun getItemCount() = dogList.size
+    override fun onDogClicked(v: View) {
+        val uuid = v.dogId.text.toString().toInt()
+        val action = ListFragmentDirections.actionDetailFragment()
+        action.dogUuid = uuid
+        Navigation.findNavController(v).navigate(action)
+
+    }
 }
